66b8116bf8a08e392bec5e9901343cd85c3c9df6
RadioMediocre <qiwen108@gmail.com>
2024-04-28 15:19:44 -0700

    Added Intro Cutscene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
-public class MainMenu : MonoBehaviour
-{
-   public void PlayGame()
-   {
-        SceneManager.LoadSceneAsync("SampleScene");
-   }
-
-   public void QuitGame()
-   {
-        Application.Quit();
-   }
-
-   public void testing()
-   {
-        Debug.Log("You Click Me!");
-   }
+public class MainMenu : MonoBehaviour{
+     public Transform MenuCanvas;
+     public void PlayGame()
+     {
+          StartCoroutine(LoadGame());
+     }
+     IEnumerator LoadGame()
+     {
+          for (int i = 0; i < 4; i++)
+          {
+               MenuCanvas.GetChild(i).gameObject.SetActive(false);
+          }
+          MenuCanvas.GetChild(4).gameObject.SetActive(true);
+          yield return new WaitForSeconds((float) MenuCanvas.GetChild(4).GetComponent<VideoPlayer>().clip.length);
+          SceneManager.LoadSceneAsync("SampleScene");
+     }
+     public void QuitGame()
+     {
+          Application.Quit();
+     }
+     public void testing()
+     {
+          Debug.Log("You Click Me!");
+     }
 }
