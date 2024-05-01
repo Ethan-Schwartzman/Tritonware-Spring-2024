1f353ef852bd5a8738f1ac00d4706b795e71c513
Ethan Schwartzman <ethancschwartzman@gmail.com>
2024-04-30 17:39:11 -0700

    Cutscene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,19 +7,23 @@ using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour{
      public Transform MenuCanvas;
+     public GameObject BackgroundImage;
+     public VideoPlayer Video;
      public void PlayGame()
      {
           StartCoroutine(LoadGame());
      }
      IEnumerator LoadGame()
      {
-          for (int i = 0; i < 4; i++)
-          {
-               MenuCanvas.GetChild(i).gameObject.SetActive(false);
-          }
-          MenuCanvas.GetChild(4).gameObject.SetActive(true);
+          //for (int i = 0; i < 4; i++)
+          //{
+          //     MenuCanvas.GetChild(i).gameObject.SetActive(false);
+          //}
+          Video.gameObject.SetActive(true);
+          BackgroundImage.SetActive(false);
+          GL.Clear(true, true, Color.black);
           float startTime = Time.deltaTime;
-          float duration = (float) MenuCanvas.GetChild(4).GetComponent<VideoPlayer>().clip.length;
+          float duration = (float) Video.clip.length + 1.5f;
           while(Time.time - startTime < duration) {
                if(Input.GetButtonDown("Menu")) break;
                yield return null;
